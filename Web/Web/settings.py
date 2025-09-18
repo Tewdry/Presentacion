68dad5b0ce605ee1425e4a089783bd1905c7175f
Gabriel Camilo <camilo.emanuel20042014@gmail.com>
2025-09-18 18:51:20 -0300

    Se añade la extensión crispy_forms

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'Portafolio',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,3 +127,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# tailwind
+
+CRISPY_TEMPLATE_PACK = 'tailwind'
